04b3870a61531c498d4e0856aed5f0243453643c
kenanbylan <kenan.baylan4654@gmail.com>
2023-03-25 16:54:29 +0300

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Kenan Baylan on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
